72e2ec9467fc59195b59a85bdd64ff40c4f3f5ce
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-01-21 21:08:00 -0800

    replicated code to make sure the program works

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string userName;
+            int userAge;
+            double productPrice;
+            bool isStudent;
+
+            Console.Write("Enter your name: ");
+            userName = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            userAge = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the price of a product: ");
+            productPrice = double.Parse(Console.ReadLine());
+
+            Console.Write("Are you a student? (true/false): ");
+            isStudent = bool.Parse(Console.ReadLine());
 
             Console.WriteLine("Hello, World!");
         }
