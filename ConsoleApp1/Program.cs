6ea406a10c2bd338db3fd1a9ce2c79d61f7fce23
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-01-21 20:48:07 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Author: Pedro Garcia
+Course: COMP003A
+Faculty: Jonathan Cruz
+Purpose: COMP-003A - Lecture Actitvity
+ */
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
