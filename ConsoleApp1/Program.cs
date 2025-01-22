4fa58ad8332611a1e4bf5d61ecd50226d273674e
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-01-21 21:15:37 -0800

    added notes to code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,22 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string userName;
-            int userAge;
-            double productPrice;
-            bool isStudent;
+            string userName; // Variable that stores user name
+            int userAge; // Variable to store age
+            double productPrice; // Variable that stores price of product
+            bool isStudent; // Variable that stores whether the user is a student
 
-            Console.Write("Enter your name: ");
-            userName = Console.ReadLine();
+            Console.Write("Enter your name: "); // Indicate to user to enter name
+            userName = Console.ReadLine(); // takes the user input
 
-            Console.Write("Enter your age: ");
-            userAge = int.Parse(Console.ReadLine());
+            Console.Write("Enter your age: "); // Indicate to user to enter age
+            userAge = int.Parse(Console.ReadLine()); // Takes the user's input
 
-            Console.Write("Enter the price of a product: ");
-            productPrice = double.Parse(Console.ReadLine());
+            Console.Write("Enter the price of a product: "); // Indicate to user to enter product price
+            productPrice = double.Parse(Console.ReadLine()); // Takes the user's input
 
-            Console.Write("Are you a student? (true/false): ");
-            isStudent = bool.Parse(Console.ReadLine());
+            Console.Write("Are you a student? (true/false): "); // indicate to user to tell whether they are a student or not
+            isStudent = bool.Parse(Console.ReadLine()); // Takes the user's input
 
             Console.WriteLine("Hello, World!");
         }
